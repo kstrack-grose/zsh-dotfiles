cba6fed305275280d8b136d10c110569c83afbf3
Eric Baer <me@ericbaer.com>
2016-04-12 15:03:39 -0700

    Add node config

---
+++ node/path.zsh
@@ -0,0 +1,6 @@
+# Export node versions
+export NVM_DIR="/Users/ebaer/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Export globally installed npm scripts
+export PATH="$(brew --prefix)/share/npm/bin:$PATH"
